87ee2692aa1ceae07323ecd095ebf64f80a6dba3
svanhesteren <s.vanhesteren@gmail.com>
2017-12-21 10:11:48 +0100

    New init

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+Todo.destroy_all
+
+Todo.create!(
+  [
+    { title: "Sweep the floor", completed: true },
+    { title: "Learn AJAX", completed: false },
+    { title: "Meditate", completed: false },
+  ],
+)
